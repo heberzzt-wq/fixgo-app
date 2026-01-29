31d064e0621c37cbab2631e54bc1695aa9a2cf02
heberzzt-wq <heberzzt@gmail.com>
2026-01-29 14:19:03 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,12 @@
-// Archivo de conexión central
+{
+  apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0",
+  authDomain: "fixgo-44e4d.firebaseapp.com",
+  projectId: "fixgo-44e4d",
+  storageBucket: "fixgo-44e4d.firebasestorage.app",
+  messagingSenderId: "1005526685116",
+  appId: "1:1005526685116:web:62f1a823ff8761da85c7b9",
+  measurementId: "G-MXNHXSY9TG"
+};// Archivo de conexión central
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
