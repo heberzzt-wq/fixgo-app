ebeaebba82d5ff0ea2d474f36ac8193ba58189ca
heberzzt-wq <heberzzt@gmail.com>
2026-01-29 20:32:26 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// Tus credenciales reales ya integradas
+// Tus credenciales reales de FixGo
 const firebaseConfig = {
   apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0",
   authDomain: "fixgo-44e4d.firebaseapp.com",
   projectId: "fixgo-44e4d",
   storageBucket: "fixgo-44e4d.firebasestorage.app",
   messagingSenderId: "1005526685116",
-  appId: "1:1005526685116:web:62f1a823ff8761da85c7b9",
-  measurementId: "G-MXNHXSY9TG"
+  appId: "1:1005526685116:web:62f1a823ff8761da85c7b9"
 };
 
-// Inicializamos Firebase con tus datos reales
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { db, storage, collection, addDoc, ref, uploadBytes, getDownloadURL };
+// Exportamos las herramientas para que otros archivos las usen
+export { db, collection, addDoc };
