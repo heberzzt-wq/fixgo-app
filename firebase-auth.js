62a9a57144e84fc3dffb1403e3446a8a694208aa
heberzzt-wq <heberzzt@gmail.com>
2026-02-01 12:58:55 -0500

    Update firebase-auth.js

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,33 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// firebase-auth.js
 import {
-  getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  GoogleAuthProvider,
   signInWithPopup,
   onAuthStateChanged,
   signOut
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0",
-  authDomain: "fixgo-44e4d.firebaseapp.com",
-  projectId: "fixgo-44e4d",
-  storageBucket: "fixgo-44e4d.firebasestorage.app",
-  messagingSenderId: "1005526685116",
-  appId: "1:1005526685116:web:62f1a823ff8761da85c7b9"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 export {
-  auth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   signInWithPopup,
-  googleProvider,
   onAuthStateChanged,
   signOut
 };
