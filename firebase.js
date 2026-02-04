dbe5f3fe44844c05aaedcce300a28d7de4bf8d35
heberzzt-wq <heberzzt@gmail.com>
2026-02-04 11:59:51 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// firebase.js
+// firebase.js - CONFIGURACIÓN CENTRAL FIXGO
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { 
     getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, 
