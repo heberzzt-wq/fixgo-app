8dd13855f0bcdc62bfb9b1ed3af4f989ff524648
heberzzt-wq <heberzzt@gmail.com>
2026-02-04 12:50:42 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,32 +1,68 @@
-// firebase.js - CONFIGURACIÓN CENTRAL FIXGO
+// Importar módulos necesarios de Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-    getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, 
-    createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { 
-    getFirestore, doc, setDoc, updateDoc, getDoc, collection, query, 
-    where, onSnapshot, orderBy, limit, addDoc, getDocs, serverTimestamp 
+import { 
+    getAuth, 
+    onAuthStateChanged, 
+    signOut, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    GoogleAuthProvider, 
+    signInWithPopup 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";  // Cambié a firebase-auth.js
+import { 
+    getFirestore, 
+    doc, 
+    setDoc, 
+    updateDoc, 
+    getDoc, 
+    collection, 
+    query, 
+    where, 
+    onSnapshot, 
+    orderBy, 
+    limit, 
+    addDoc, 
+    getDocs, 
+    serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// Datos de configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0",
-  authDomain: "fixgo-44e4d.firebaseapp.com",
-  projectId: "fixgo-44e4d",
-  storageBucket: "fixgo-44e4d.appspot.com",
-  messagingSenderId: "1005526685116",
-  appId: "1:1005526685116:web:62f1a823ff8761da85c7b9",
-  measurementId: "G-MXNHXSY9TG"
+  apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0",
+  authDomain: "fixgo-44e4d.firebaseapp.com",
+  projectId: "fixgo-44e4d",
+  storageBucket: "fixgo-44e4d.appspot.com",  // Corregí la URL del storage bucket
+  messagingSenderId: "1005526685116",
+  appId: "1:1005526685116:web:62f1a823ff8761da85c7b9",
+  measurementId: "G-MXNHXSY9TG"
 };
 
+// Inicialización
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
-const googleProvider = new GoogleAuthProvider();
 
-export { 
-    auth, db, googleProvider, onAuthStateChanged, signOut, 
-    signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, 
-    doc, setDoc, updateDoc, getDoc, collection, query, where, 
-    onSnapshot, orderBy, limit, addDoc, serverTimestamp, getDocs
+// Exportación para que login.html y app-admin.js funcionen
+export { 
+    auth, 
+    db, 
+    GoogleAuthProvider, 
+    onAuthStateChanged, 
+    signOut, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    signInWithPopup, 
+    doc, 
+    setDoc, 
+    updateDoc, 
+    getDoc, 
+    collection, 
+    query, 
+    where, 
+    onSnapshot, 
+    orderBy, 
+    limit, 
+    addDoc, 
+    serverTimestamp, 
+    getDocs 
 };
