e3873ac620b1a1de2ff94f82ac5b3ab458aafd04
heberzzt-wq <heberzzt@gmail.com>
2026-02-03 21:28:36 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,43 +1,45 @@
-// firebase.js
+// firebase.js - Módulo Centralizado FixGo
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { 
-    getFirestore, doc, setDoc, updateDoc, getDoc, 
-    collection, query, where, onSnapshot, orderBy, 
-    limit, addDoc, serverTimestamp, 
-    getDocs // <--- AGREGA ESTO AQUÍ
+    getAuth, 
+    onAuthStateChanged, 
+    signOut, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    GoogleAuthProvider, 
+    signInWithPopup 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { 
+    getFirestore, 
+    doc, 
+    setDoc, 
+    updateDoc, 
+    getDoc, 
+    collection, 
+    query, 
+    where, 
+    onSnapshot, 
+    orderBy, 
+    limit, 
+    addDoc, 
+    getDocs, // Aseguramos que getDocs esté aquí para el admin
+    serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-// ... (tu config de firebaseConfig) ...
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { 
-    auth, db, GoogleAuthProvider, onAuthStateChanged, signOut, 
-    signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, 
-    doc, setDoc, updateDoc, getDoc, collection, query, where, 
-    onSnapshot, orderBy, limit, addDoc, serverTimestamp,
-    getDocs // <--- Y AGREGA ESTO AQUÍ TAMBIÉN
-};
-
 
 const firebaseConfig = {
     apiKey: "AIzaSyBlE0bkNxYC3w7KG7t9D2NU-Q3jh3B5H7k",
-    authDomain: "fixgo-f1665.firebaseapp.com",
-    projectId: "fixgo-f1665",
-    storageBucket: "fixgo-f1665.appspot.com",
+    authDomain: "fixgo-44e4d.firebaseapp.com",
+    projectId: "fixgo-44e4d",
+    storageBucket: "fixgo-44e4d.appspot.com",
     messagingSenderId: "36531388043",
     appId: "1:36531388043:web:573f00199f7d4668744093"
 };
 
-// Inicializar
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// INICIALIZACIÓN ÚNICA (Línea 36 corregida)
+const firebaseApp = initializeApp(firebaseConfig); 
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
 
-// Exportación corregida: Exportamos la CLASE GoogleAuthProvider, no una instancia.
 export { 
     auth, 
     db, 
@@ -58,5 +60,6 @@ export {
     orderBy, 
     limit, 
     addDoc, 
+    getDocs,
     serverTimestamp 
 };
