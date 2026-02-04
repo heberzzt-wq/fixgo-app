772fafd9834e75dd83127f40a17897550cd600c6
heberzzt-wq <heberzzt@gmail.com>
2026-02-04 10:47:31 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
 // firebase.js - CONFIGURACIÓN FINAL FIXGO
+
+// Importar módulos necesarios de Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { 
     getAuth, 
@@ -8,7 +10,7 @@ import {
     createUserWithEmailAndPassword, 
     GoogleAuthProvider, 
     signInWithPopup 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";  // Cambié a firebase-auth.js
 import { 
     getFirestore, 
     doc, 
@@ -26,12 +28,12 @@ import {
     serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Estos datos son los de tu Captura 368
+// Datos de configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0",
   authDomain: "fixgo-44e4d.firebaseapp.com",
   projectId: "fixgo-44e4d",
-  storageBucket: "fixgo-44e4d.firebasestorage.app",
+  storageBucket: "fixgo-44e4d.appspot.com",  // Corregí la URL del storage bucket
   messagingSenderId: "1005526685116",
   appId: "1:1005526685116:web:62f1a823ff8761da85c7b9",
   measurementId: "G-MXNHXSY9TG"
@@ -44,8 +46,25 @@ const db = getFirestore(firebaseApp);
 
 // Exportación para que login.html y app-admin.js funcionen
 export { 
-    auth, db, GoogleAuthProvider, onAuthStateChanged, signOut, 
-    signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, 
-    doc, setDoc, updateDoc, getDoc, collection, query, where, 
-    onSnapshot, orderBy, limit, addDoc, serverTimestamp, getDocs
+    auth, 
+    db, 
+    GoogleAuthProvider, 
+    onAuthStateChanged, 
+    signOut, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    signInWithPopup, 
+    doc, 
+    setDoc, 
+    updateDoc, 
+    getDoc, 
+    collection, 
+    query, 
+    where, 
+    onSnapshot, 
+    orderBy, 
+    limit, 
+    addDoc, 
+    serverTimestamp, 
+    getDocs 
 };
