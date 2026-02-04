ddd33c64b13f532133e1c9feed8de1bcb5225f14
heberzzt-wq <heberzzt@gmail.com>
2026-02-04 11:23:20 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,70 +1,32 @@
-// firebase.js - CONFIGURACIÓN FINAL FIXGO
-
-// Importar módulos necesarios de Firebase
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { 
-    getAuth, 
-    onAuthStateChanged, 
-    signOut, 
-    signInWithEmailAndPassword, 
-    createUserWithEmailAndPassword, 
-    GoogleAuthProvider, 
-    signInWithPopup 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";  // Cambié a firebase-auth.js
+    getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { 
-    getFirestore, 
-    doc, 
-    setDoc, 
-    updateDoc, 
-    getDoc, 
-    collection, 
-    query, 
-    where, 
-    onSnapshot, 
-    orderBy, 
-    limit, 
-    addDoc, 
-    getDocs, 
-    serverTimestamp 
+    getFirestore, doc, setDoc, updateDoc, getDoc, collection, query, 
+    where, onSnapshot, orderBy, limit, addDoc, getDocs, serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Datos de configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0",
   authDomain: "fixgo-44e4d.firebaseapp.com",
   projectId: "fixgo-44e4d",
-  storageBucket: "fixgo-44e4d.appspot.com",  // Corregí la URL del storage bucket
+  storageBucket: "fixgo-44e4d.appspot.com",
   messagingSenderId: "1005526685116",
   appId: "1:1005526685116:web:62f1a823ff8761da85c7b9",
   measurementId: "G-MXNHXSY9TG"
 };
 
-// Inicialización
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
+const googleProvider = new GoogleAuthProvider();
 
-// Exportación para que login.html y app-admin.js funcionen
 export { 
-    auth, 
-    db, 
-    GoogleAuthProvider, 
-    onAuthStateChanged, 
-    signOut, 
-    signInWithEmailAndPassword, 
-    createUserWithEmailAndPassword, 
-    signInWithPopup, 
-    doc, 
-    setDoc, 
-    updateDoc, 
-    getDoc, 
-    collection, 
-    query, 
-    where, 
-    onSnapshot, 
-    orderBy, 
-    limit, 
-    addDoc, 
-    serverTimestamp, 
-    getDocs 
+    auth, db, googleProvider, onAuthStateChanged, signOut, 
+    signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, 
+    doc, setDoc, updateDoc, getDoc, collection, query, where, 
+    onSnapshot, orderBy, limit, addDoc, serverTimestamp, getDocs
 };
