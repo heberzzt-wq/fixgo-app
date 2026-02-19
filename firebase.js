462607ed314aa9bfe47e3662e95d26568aeb1e08
heberzzt-wq <heberzzt@gmail.com>
2026-02-19 13:09:51 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -31,7 +31,7 @@ import {
 
 // Configuración de credenciales
 const firebaseConfig = {
-    apiKey: "AIzaSyBlE0bkNxYC3w7KG7t9D2NU-Q3jh3B5H7k", 
+    apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0", 
     authDomain: "fixgo-44e4d.firebaseapp.com",
     projectId: "fixgo-44e4d",
     storageBucket: "fixgo-44e4d.appspot.com",
