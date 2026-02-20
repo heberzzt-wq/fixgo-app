4baadf9ae7005e4ebcc9250376fb91cbb8605f21
heberzzt-wq <heberzzt@gmail.com>
2026-02-19 23:22:49 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -35,7 +35,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCmZRLFPWnJFMYvcYXhwQ-CyNU5rz3z9V0", 
     authDomain: "fixgo-44e4d.firebaseapp.com",
     projectId: "fixgo-44e4d",
-    storageBucket: "fixgo-44e4d.appspot.com",
+    storageBucket: "fixgo-44e4d.firebasestorage.app",
     messagingSenderId: "1005526685116",
     appId: "1:1005526685116:web:62f1a823ff8761da85c7b9"
 };
