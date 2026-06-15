90a792051e3c2800ca9e9d67ff1c0ecf5439abe4
Heberto Mendoza <heberzzt@gmail.com>
2026-06-15 13:17:00 -0500

    j,j,n,nm,n,

--- test-replace.js
+++ test-replace.js
@@ -1,25 +1,16 @@
 /* fixgo_safe_edit_start */
 
-
-
 function a() {
 
     return {
         ok: true,
-        timestamp: date.now()
-    };
-}
-
-
-
-
-function runtimelatency() {
-
-    return {
-        repaired: true,
         timestamp: Date.now()
     };
-};
-};
 }
- /* fixgo_safe_edit_end */                                                                                                                                                                   
+
+export function runtimelatency() {
+
+    return 0;
+}
+
+/* fixgo_safe_edit_end */
