3064c024500dd9ad6a501d2658e2cd8f5f41149a
Heberto Mendoza <heberzzt@gmail.com>
2026-07-15 01:43:13 -0500

    Refresh grounded marketing runtime

--- gestia-core/tools.runtime.js
+++ gestia-core/tools.runtime.js
@@ -5,7 +5,7 @@
 
 import {
     registerJarvisMultifunctionTools
-} from "./jarvis/jarvis.multitool.pack.js?v=sia7-multifunction-tools-v5-structured-media-plans-20260715";
+} from "./jarvis/jarvis.multitool.pack.js?v=sia7-multifunction-tools-v6-grounded-marketing-20260715";
 import {
     registerJarvisActuatorTools
 } from "./jarvis/jarvis.actuator.pack.js?v=sia7-real-actuators-v3.6-pdf-visual-20260714";
