5f568592ddc0c090cd3ea967186e08ba02877eca
Heberto Mendoza <heberzzt@gmail.com>
2026-09-08 00:45:10 -0500

    SIA7 result v142-humo-worker-result-duration-20260908-235

--- jarvis-github-worker.js
+++ jarvis-github-worker.js
@@ -678,7 +678,7 @@ async function executeHuMoIdentityProbeJob(job = {}) {
         hardBudgetUsd,
         monthlyStorageAuthorizedUsd,
         characterId: result.characterId,
-        durationSeconds: 8.0,
+        durationSeconds: 3.88,
         referenceCount: 2,
         referenceSha256s: [SIA7_HUMO_REFERENCE_SHA256, SIA7_HUMO_REFERENCE_SHA256_2],
         audioSha256: SIA7_HUMO_AUDIO_SHA256,
